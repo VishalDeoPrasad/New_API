3a9104fde6a1a329f9564e3d0c317cf32ada8a39
vishaldeoprasad <vishaldeoprasad@yahoo.com>
2023-11-10 15:54:37 +0530

    update

--- internal/models/company.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "gorm.io/gorm"
-
-type Company struct {
-	gorm.Model
-	Name     string `json:"name" validate:"required"`
-	Location string `json:"location" validate:"required"`
-}
