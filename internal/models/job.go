899571d323e5aa59d9178ff6e70e2ed4072c8102
vishaldeoprasad <vishaldeoprasad@yahoo.com>
2023-11-10 15:54:47 +0530

    job service

--- internal/models/job.go
+++ internal/models/job.go
@@ -2,6 +2,12 @@ package models
 
 import "gorm.io/gorm"
 
+type Company struct {
+	gorm.Model
+	Name     string `json:"name" validate:"required"`
+	Location string `json:"location" validate:"required"`
+}
+
 type Job struct {
 	gorm.Model
 	Company         Company           `json:"-" gorm:"ForeignKey:cid"`
